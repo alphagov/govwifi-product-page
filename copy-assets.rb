2ba98c87ec215fc246d3a03139076b1ea972514c
Stéphane Maniaci <stephane.maniaci@digital.cabinet-office.gov.uk>
2020-02-20 13:25:13 +0000

    fix: remove a ton of useless JS
    We don't want to use these GOV.UK Modules anymore, and the custom
    Navigation module code to add the ARIA attribute is already done by
    the new GOV.UK Frontend, I've checked.
    
    And no JQuery! Who would have thought?

--- copy-assets.rb
+++ copy-assets.rb
@@ -6,17 +6,19 @@ GOVUK_FRONTEND_PATH = './node_modules/govuk-frontend/govuk'.freeze
 OUR_PATH = './source'.freeze
 
 ASSETS_MAP = {
-  "#{GOVUK_FRONTEND_PATH}/assets" => 'assets',
+  "#{GOVUK_FRONTEND_PATH}/assets" => '',
   "#{GOVUK_FRONTEND_PATH}/all.js" => 'javascripts/dist/',
   'node_modules/gaap-analytics/build/gaap-analytics.min.js' => 'javascripts/dist/'
 }.freeze
 
+puts "[copy-assets.rb] Starting assets copy.\n\n"
+
 ASSETS_MAP.each do |src, dest_folder|
   destination = File.join(OUR_PATH, dest_folder)
 
-  puts "[copy-assets.rb] copying #{src} over to #{destination}"
+  Dir.mkdir(destination) unless Dir.exist?(destination)
 
-  FileUtils.cp_r src, destination
-
-  puts "[copy-assets.rb] successfully copied #{src} into #{destination}"
+  FileUtils.cp_r(src, destination, verbose: true)
 end
+
+puts "\n[copy-assets.rb] Assets copy finished."
--- source/javascripts/start-modules.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-  GOVUKFrontend.initAll();
-});

290b72b2b1b852c06de01021bd62b1c235eb0f2b
Stéphane Maniaci <stephane.maniaci@digital.cabinet-office.gov.uk>
2020-11-19 16:23:54 +0000

    fix: add acceptance testing framework
    Using Capybara + Puma + Selenium.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+require 'capybara/rspec'
+
+require 'middleman-core'
+require 'middleman-core/rack'
+
+require 'webdrivers/chromedriver'
+Capybara.default_driver = :selenium_chrome_headless
+
+middleman_app = ::Middleman::Application.new do
+  set :root, File.expand_path(File.join(File.dirname(__FILE__), '..'))
+  set :environment, :development
+  set :show_exceptions, false
+end
+
+Capybara.enable_aria_label = true
+
+Capybara.app = ::Middleman::Rack.new(middleman_app).to_app
